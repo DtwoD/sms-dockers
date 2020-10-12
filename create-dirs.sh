cacbdda407c1a6b5579d68f5398b6a1625d88f96
Jhony Vidal <55899088+sms-joliveira@users.noreply.github.com>
2020-10-12 12:00:07 -0300

    Add Network Bridge and SonarQube with PostgreSQL integration

--- create-dirs.sh
+++ create-dirs.sh
@@ -3,5 +3,9 @@
 mkdir -p ~/docker/mule-ee/apps
 mkdir -p ./containers/jenkins/data
 mkdir -p ./containers/nexus/data
-chmod a+w containers/jenkins/data
-chmod a+w containers/nexus/data
+mkdir -p ./containers/sonarqube/data
+mkdir -p ./containers/postgresql/data
+chmod a+w ./containers/jenkins/data
+chmod a+w ./containers/nexus/data
+chmod a+w ./containers/sonarqube/data
+chmod a+w ./containers/postgresql/data
